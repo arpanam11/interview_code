6169398a22e429b78f5b9acc29d640de1d8e63c4
arpanam11 <arpanameshram11@gmail.com>
2024-08-22 12:05:42 +0530

    get APi data

--- mycode/src/React/Getapi.js
+++ mycode/src/React/Getapi.js
@@ -1,15 +1,35 @@
-import React from 'react'
-//https://fakestoreapi.com/products
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 const Getapi = () => {
 
-    Getdata= () => ({
+    const [data, setData] = useState([]);
 
-        const response = axios.get(`https://fakestoreapi.com/products`)
-        console.log(response.data)
-    })
-  return (
-    <div>Getapi</div>
-  )
-}
+    const Getdata = async () => {
+        try {
+            const response = await axios.get('https://fakestoreapi.com/products');
+            setData(response.data); // Set the fetched data to state
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
 
-export default Getapi
+    useEffect(() => {
+        Getdata();
+    }, []); 
+
+    return (
+        <div>
+            <h1>Products</h1>
+            <ul>
+                {data.map((item) => (
+                    <li key={item.id}>
+                        {item.title} - ${item.price}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default Getapi;
