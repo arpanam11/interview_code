52916c4b5911c13f5f76653a00cefaddf38e09b6
arpanam11 <arpanameshram11@gmail.com>
2024-08-22 12:04:34 +0530

    axios

---
+++ mycode/src/React/Getapi.js
@@ -0,0 +1,15 @@
+import React from 'react'
+//https://fakestoreapi.com/products
+const Getapi = () => {
+
+    Getdata= () => ({
+
+        const response = axios.get(`https://fakestoreapi.com/products`)
+        console.log(response.data)
+    })
+  return (
+    <div>Getapi</div>
+  )
+}
+
+export default Getapi
---
+++ mycode/src/React/propsState.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const propsState = () => {
+  return (
+    <div>propsState</div>
+  )
+}
+
+export default propsState
